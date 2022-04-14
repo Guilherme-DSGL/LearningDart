001615ebb90b876d3b127ae0dcd755d4317217eb
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2022-04-14 20:53:31 -0300

    Create variaveis.dart

---
+++ Fundamentos/variaveis.dart
@@ -0,0 +1,19 @@
+main() {
+  //dinamicos
+  dynamic alfa;
+  var delta; //inferencia
+  //numericos
+  int a = 2;
+  double b = 3.14;
+  num e = 6;
+  //String
+  String c = 'C';
+  //Booleano
+  bool d = true;
+  //Listas
+  List f = ['Ana', 'Guilherme'];
+  Map<String, String> g = {
+    'name': 'João',
+  };
+  Set h = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+}
