42c3e78fa7fa4ade9780d36e4afee01c414a402c
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2022-04-16 20:00:56 -0300

    Update exercicio_3.dart

--- Exercicios/exercicio_3.dart
+++ Exercicios/exercicio_3.dart
@@ -1,15 +1,15 @@
-exportProduto({required String name, required double price}) {
+exportProduct({required String name, required double price}) {
   print("${name}, ${price}");
 }
 
-class Produto {
+class Product {
   String name;
   double price;
 
-  Produto({required this.name, required this.price});
+  Product({required this.name, required this.price});
 }
 
 main() {
-  var p1 = new Produto(name: 'bag', price: 99.99);
-  exportProduto(name: p1.name, price: p1.price);
+  var p1 = new Product(name: 'bag', price: 99.99);
+  exportProduct(name: p1.name, price: p1.price);
 }
